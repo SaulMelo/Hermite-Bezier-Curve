3893ec9e8efedfce7a9f588ca27a191e793031c3
ktd <ktd@10.100.43.148>
2013-09-16 20:06:57 -0700

    Comments

--- src/graphics2/Assignment2.java
+++ src/graphics2/Assignment2.java
@@ -17,12 +17,13 @@ public class Assignment2 {
         int image_width = 600;
         int image_height = 600;
 
-        System.out.println("Creating Hermite Curve ...");
+        System.out.println("Processing Hermite Curve ...");
         
         Hermite hermite = new Hermite(image_width, image_height, 0, 0, 0);
         hermite.two(128, 495, 313, 493, 12, 120, 474, 124);
         hermite.write("/Users/ktd/Desktop/hermite.png");
         
+        System.out.println("Processing Bezier Curve ...");
         Bezier bezier = new Bezier(image_width, image_height, 0, 0, 0);
         bezier.two(128, 495, 313, 493, 12, 120, 474, 124);
         bezier.write("/Users/ktd/Desktop/bezier.png");        
